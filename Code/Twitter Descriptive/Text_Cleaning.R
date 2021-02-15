1631d1471da53c6500ca6a6221ae93461188bb69
Lukas Brunner <lukas.brunner@student.uni-tuebingen.de>
2021-02-15 11:36:56 +0100

    Update Text_Cleaning.R
    remove stopwords directly

--- Code/Twitter Descriptive/Text_Cleaning.R
+++ Code/Twitter Descriptive/Text_Cleaning.R
@@ -158,13 +158,14 @@ stem_hunspell <- function(term) {
   stem
 }
 tweets$text <- text_tokens(tweets$text, stemmer = stem_hunspell)
-# add list to one string again
+# collapse text column list to one string again
 tweets <-tweets %>% rowwise() %>% 
   mutate(text = paste(text, collapse=' ')) %>%
   ungroup()
 
 
 ###### place column contains lists
+# move coordinates into two lat/long columns
 tweets <- unnest_wider(tweets, place) %>%
   select(-c("...1", "type")) %>%
   mutate_all(list(~na_if(.,"NULL"))) %>%
@@ -173,8 +174,12 @@ tweets <- unnest_wider(tweets, place) %>%
 
 
 
+# remove stopwords
+tweets$text <- removeWords(tweets$text,stopwords("SMART"))
 
-
+# remove whitespace again
+#get rid of unnecessary white spaces
+tweets$text <- stringr::str_squish(tweets$text)
 
 
 
@@ -191,44 +196,3 @@ feather::write_feather(tweets, path)
 
 
 
-#set the schema:docs
-docs <- tm::DataframeSource(tweets)
-
-
-
-
-#clean_corpus function from datacamp
-clean_corpus <- function(corpus){
-  #corpus <- tm_map(corpus,  content_transformer(replace_abbreviation))
-  corpus <- tm_map(corpus, removePunctuation)
-  
-  corpus <- tm_map(corpus, removeNumbers)
-  corpus <- tm_map(corpus,
-                   content_transformer(tolower))
-  corpus <- tm_map(corpus, removeWords,
-                   c(stopwords("SMART"), "amp"))
-  corpus <- tm_map(corpus, stripWhitespace)
-  corpus <- tm_map(corpus,  content_transformer(replace_abbreviation))
-  return(corpus)
-}
-
-text_corpus <- clean_corpus(VCorpus(docs))
-
-
-
-content(text_corpus[[1]])
-#for standard meta data (including id)
-meta(text_corpus[[1]])
-
-#for all added meta data
-meta(text_corpus[1])
-
-dtm <- DocumentTermMatrix(text_corpus)
-#remove sparse words
-dtm <- removeSparseTerms(dtm, 0.99)
-dtm_m <- as.matrix(dtm)
-
-#check matrix
-dtm_m[1,]
-
-rm(docs, dtm, text_corpus, tweets_raw, clean_corpus)
