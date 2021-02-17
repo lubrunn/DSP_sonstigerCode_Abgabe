e93615a026b61aa770a9eaff1a5004d1a8bd7e50
Lukas Brunner <lukas.brunner@student.uni-tuebingen.de>
2021-02-17 16:29:39 +0100

    Update wordcloud.R
    added different wordcloud lib

--- Code/Twitter Descriptive/wordcloud.R
+++ Code/Twitter Descriptive/wordcloud.R
@@ -1,4 +1,4 @@
-source("Text_Cleaning.R")
+source("text_to_dtm.R")
 library(wordcloud)
 
 #Sum columns and sort by frequency
@@ -8,3 +8,15 @@ word_freqs <- data.frame(term = names(term_frequency),
 # Make word cloud
 wordcloud(word_freqs$term, word_freqs$num,
           max.words = 100, colors = "red")
+
+
+#### different wordcloud
+devtools::install_github("lchiffon/wordcloud2")
+
+
+library(wordcloud2)
+term_frequency_df <- data.frame("freq" = term_frequency) %>% rownames_to_column(var = "word")
+
+wordcloud2(term_frequency_df, size = 1,shape = 'star',
+           color = "random-light", backgroundColor = "grey")
+a <- demoFreq
