702d62b3ac0a056f235c2fb39fa190dbf44f57e4
Lukas Brunner <lukas.brunner@student.uni-tuebingen.de>
2021-02-18 20:24:06 +0100

    Update word_freq.R
    removed base r plot

--- Code/Twitter Descriptive/word_freq.R
+++ Code/Twitter Descriptive/word_freq.R
@@ -1,18 +1,17 @@
 source("text_to_dtm.R")
 
 # Sum rows and sort by frequency
-term_frequency <- colSums(dtm_m_filt)
-term_frequency <- sort(term_frequency,
-                       decreasing = TRUE)
-# Create a barplot
-barplot(term_frequency[1:10],
-        las = 2,
-        col = "white",
-        main = "Word Frequencies")
+
+term_frequency <- colSums(dtm_m)
+
+
+# only for already filtered tweets
+# term_frequency <- colSums(dtm_m_filt)
+
 
 word_count <- data.frame("n" = term_frequency) %>% rownames_to_column("word")
 word_count %>%
-  top_n(10) %>% 
+  top_n(20) %>% 
   arrange(desc(n)) %>%
 ggplot(aes(x = word, y = n)) +
   geom_col() +
