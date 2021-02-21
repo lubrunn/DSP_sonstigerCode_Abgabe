3b01c2b3df96c967fee87ba3c98b8fc63af6d505
Lukas Brunner <lukas.brunner@student.uni-tuebingen.de>
2021-02-21 15:30:21 +0100

    Update read_json.py
    created function for df cleaning

--- Code/Twitter Descriptive/read_json.py
+++ Code/Twitter Descriptive/read_json.py
@@ -5,42 +5,64 @@ import pandas as pd
 import re
 import numpy as np
 from datetime import datetime
+import swifter
 #import pyarrow
 
-
+import demoji
+#demoji.download_codes()
 
 #%%
-# tweets = []
-# path = os.path.join("test.json")
-# for line in open(path, 'r',encoding="utf8"):
-#     tweets.append(json.loads(line,parse_int=str))
+        
+def df_cleaner(df):        
+    # drop duplicate tweets
+    df = df.drop_duplicates(subset=["id"], keep='first')
+
+    # only keep tweets with correct language
+    df = df[df["language"] == lang.lower()]
+    # split coordingates column into lat/long
+    if ~sum(df["place"] == "") == len(df):
+        coord = df["place"].apply(pd.Series)["coordinates"].swifter.progress_bar(False).apply(pd.Series)
+        coord.rename(columns = {0:"lat", 1:"long"},
+             inplace = True)
+        df = pd.concat([df, coord], axis = 1)
+    
+    else:
+        df["lat"] = df["long"] = None
+        df.drop("place", axis = 1, inplace = True)
+                
+            
+    # only select needed columns
+    df = df[["id", "tweet", "created_at", "user_id", 
+             "username", "hashtags", "lat", "long", 
+             "language", "replies_count", "retweets_count", 
+             "likes_count"]]
+    
+    df.reset_index(inplace = True, drop = True)
+    
+    # remove links and handles
+    df["tweet"] = df["tweet"].swifter.progress_bar(False).apply(lambda tweet: remove_light(tweet))
+    # strip extra whitespace
+    df["tweet"] = df["tweet"].str.strip()
+    df["tweet"] = df["tweet"].replace('\s+', ' ', regex=True)
     
     
-# # with open('data.json', 'w') as outfile:
-# #     json.dump(tweets, outfile)
-
-# #%%    
-# df = pd.DataFrame(tweets)
-# # split place column into two column
-# coord = df["place"].apply(pd.Series)["coordinates"].apply(pd.Series)
-# coord.rename(columns = {0:"lat", 1:"long"},
-#              inplace = True)
-
-# df.drop("place", axis = 1, inplace = True)
-
-# df = pd.concat([df, coord], axis = 1)
-
-# #%%
-# df.to_feather("test.feather")
-
+    # compute length of tweets
+    length_checker = np.vectorize(len)
+    # get length of each tweet
+    df["tweet_length"] = length_checker(df["tweet"])
+    
+    # convert tweet_length to object
+    df["tweet_length"] = df["tweet_length"].astype("object")
+    return df
 
 #%% get folder names
 source = "raw_test"
-dest = "raw_feather"
-folders = [k for k in os.listdir(source) if "Comp" in k or "Filter" in k]
+dest = "raw_csv"
+dest_cleaned = "pre_cleaned"
+folders_all = [k for k in os.listdir(source) if "Comp" in k or "Filter" in k]
 
-company_folders = [k for k in folders if "Comp" in k]
-nofilter_folders = [k for k in folders if "Filter" in k]
+company_folders = [k for k in folders_all if "Comp" in k]
+nofilter_folders = [k for k in folders_all if "Filter" in k]
 
 #%% testing
 folder = nofilter_folders[0]
@@ -49,46 +71,54 @@ folder = company_folders[0]
 subfolder = subfolders[0]
 
 #%%
-for folder in company_folders:
-
-    subfolders = os.listdir(os.path.join(source,folder))
-    for subfolder in subfolders:
-        new_dest = os.path.join(dest,folder, subfolder)
-
-        if not os.path.exists(new_dest):
-            os.mkdir(os.path.join(new_dest))
-        files = os.listdir(os.path.join(source,folder, subfolder))
-        for file in [k for k in files if ".json" in k]:
-            
-            tweets = []
-            path = os.path.join(source,folder,subfolder, file)
-            for line in open(path, 'r',encoding="utf8"):
-                tweets.append(json.loads(line,parse_int=str))
-            df = pd.DataFrame(tweets)
-            
-            # fix place column
-            # split into two new lat/long columns
-            if ~sum(df["place"] == "") == len(df):
+for main_folder in folders_all:
+    if main_folder in company_folders:
+        subfolders = os.listdir(os.path.join(source,folder))
+        for subfolder in subfolders:
+            new_dest = os.path.join(dest,folder, subfolder)
+    
+            if not os.path.exists(new_dest):
+                os.mkdir(os.path.join(new_dest))
+            files = os.listdir(os.path.join(source,folder, subfolder))
+            for file in [k for k in files if ".json" in k]:
                 
-                coord = df["place"].apply(pd.Series)["coordinates"].apply(pd.Series)
-                coord.rename(columns = {0:"lat", 1:"long"},
-                     inplace = True)
-                df = pd.concat([df, coord], axis = 1)
+                tweets = []
+                path = os.path.join(source,folder,subfolder, file)
+                for line in open(path, 'r',encoding="utf8"):
+                    tweets.append(json.loads(line,parse_int=str))
+                df = pd.DataFrame(tweets)
+                
+                # fix place column
+                # split into two new lat/long columns
+                if ~sum(df["place"] == "") == len(df):
+                    
+                    coord = df["place"].apply(pd.Series)["coordinates"].swifter.progress_bar(False).apply(pd.Series)
+                    coord.rename(columns = {0:"lat", 1:"long"},
+                         inplace = True)
+                    df = pd.concat([df, coord], axis = 1)
+            
+                else:
+                    df["lat"] = df["long"] = None
+                df.drop("place", axis = 1, inplace = True)
+                
+                
+                # only select needed columns
+                df = df[["id", "tweet", "created_at", "user_id", 
+                         "username", "hashtags", "lat", "long", 
+                         "language", "replies_count", "retweets_count", 
+                         "likes_count"]]
+                
+                
+                new_filename = f"{file.split('.')[0]}.csv"
+                df.to_csv(os.path.join(new_dest,new_filename))    
+                
         
-            else:
-                df["lat"] = df["long"] = None
-            df.drop("place", axis = 1, inplace = True)
             
-            
-            # only select needed columns
-            df = df[["id", "tweet", "created_at", "user_id", 
-                     "username", "hashtags", "lat", "long", 
-                     "language", "replies_count", "retweets_count", 
-                     "likes_count"]]
-            
-            
-            new_filename = f"{file.split('.')[0]}.feather"
-            df.to_feather(os.path.join(new_dest,new_filename))
+    
+    
+
+
+    
         
     
             
@@ -132,7 +162,7 @@ def remove_light(text):
 print(remove_light('''Everybody in the UK is flipping out over a video of some 
                    dude picking up a mace, and I cannot for the life of me 
                    understand why.   Is it because you’re playing by 1st ed. 
-                   rules and he’s not a cleric?'''))
+                   rules and he’s not a cleric? lukas@email.com @donaldtup'''))
 
    
 
@@ -144,6 +174,7 @@ for lang in lang_folders:
         folders = de_folders
     else:
         folders = en_folders
+    
         
     # go thru all dates
     for date in date_list_needed:
@@ -164,51 +195,26 @@ for lang in lang_folders:
         # convert to df
         df = pd.DataFrame(tweets)
         
-        # drop duplicate tweets
-        df = df.drop_duplicates(subset=["id"], keep='first')
-    
-        # only keep tweets with correct language
-        df = df[df["language"] == lang.lower()]
-        # split coordingates column into lat/long
-        if ~sum(df["place"] == "") == len(df):
-            coord = df["place"].apply(pd.Series)["coordinates"].apply(pd.Series)
-            coord.rename(columns = {0:"lat", 1:"long"},
-                 inplace = True)
-            df = pd.concat([df, coord], axis = 1)
-        
-        else:
-            df["lat"] = df["long"] = None
-            df.drop("place", axis = 1, inplace = True)
-                    
-                
-        # only select needed columns
-        df = df[["id", "tweet", "created_at", "user_id", 
-                 "username", "hashtags", "lat", "long", 
-                 "language", "replies_count", "retweets_count", 
-                 "likes_count"]]
-        
-        df.reset_index(inplace = True, drop = True)
-        
-        # remove links and handles
-        df["tweet"] = df["tweet"].apply(lambda tweet: remove_light(tweet))
-        # strip extra whitespace
-        df["tweet"] = df["tweet"].str.strip()
-        df["tweet"] = df["tweet"].replace('\s+', ' ', regex=True)
-        
-        
-        # compute length of tweets
-        length_checker = np.vectorize(len)
-        # get length of each tweet
-        df["tweet_length"] = length_checker(df["tweet"])
-        
-        # convert tweet_length to object
-        df["tweet_length"] = df["tweet_length"].astype("object")
+        # clean dataframe
+        df = df_cleaner(df)
         
         
         
         new_filename = f"{lang}_NoFilter_{date}.feather"
         new_filename_csv = f"{lang}_NoFilter_{date}.csv"
-        # version 1.0 for readability in R
-        df.to_feather(os.path.join(dest, f"{lang}_NoFilter",new_filename))
+        
+        # save df
         df.to_csv(os.path.join(dest, f"{lang}_NoFilter",new_filename_csv),
+                  index = False)
+        
+        
+        # now replace emojis and save in different destination
+        df["tweet"] = df["tweet"].swifter.progress_bar(False).apply(lambda tweet: demoji.replace_with_desc(tweet, 
+                                                                                           sep = " "))
+        
+        # replace _ from emojis with " "
+        df.tweet = df.tweet.str.replace("_", " ")
+        
+        #save df
+        df.to_csv(os.path.join(dest_cleaned, f"{lang}_NoFilter",new_filename_csv),
                   index = False)
