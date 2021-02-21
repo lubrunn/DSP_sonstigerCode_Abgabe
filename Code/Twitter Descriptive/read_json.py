d79aefca4c7a900676995eb3e0a1d81dbd9e9116
Lukas Brunner <lukas.brunner@student.uni-tuebingen.de>
2021-02-21 20:20:43 +0100

    Update read_json.py
    added funcitonnality to read and clean all company files, goes thru each company folder an merges german and english files togehter, if only german or english available only that file is cleaned, resutls are stored new folders

--- Code/Twitter Descriptive/read_json.py
+++ Code/Twitter Descriptive/read_json.py
@@ -13,12 +13,15 @@ import demoji
 
 #%%
         
-def df_cleaner(df):        
+def df_cleaner(df, lang_controller = False):        
     # drop duplicate tweets
     df = df.drop_duplicates(subset=["id"], keep='first')
-
-    # only keep tweets with correct language
-    df = df[df["language"] == lang.lower()]
+    
+    if lang_controller == True:
+        # only keep tweets with correct language
+        df = df[df["language"] == lang.lower()]
+    else:
+        df = df[df["language"].isin(["en", "de"])]
     # split coordingates column into lat/long
     if ~sum(df["place"] == "") == len(df):
         coord = df["place"].apply(pd.Series)["coordinates"].swifter.progress_bar(False).apply(pd.Series)
@@ -52,8 +55,18 @@ def df_cleaner(df):
     df["tweet_length"] = length_checker(df["tweet"])
     
     # convert tweet_length to object
-    df["tweet_length"] = df["tweet_length"].astype("object")
+    df["tweet_length"] = df["tweet_length"]
+    
+    # drop any missing tweets
+    df = df.dropna(subset=["tweet"])
     return df
+
+
+#%% cleaning functions
+def remove_light(text):
+        reference_pattern = re.compile(r"\@\w+|\@\w+\'|\@\w+\’|<.*?>|https?://\S+|www\.\S+|\d+\.|\d+|\#|\&amp|RT")
+        return reference_pattern.sub(r'', text)
+
 
 #%% get folder names
 source = "raw_test"
@@ -61,8 +74,14 @@ dest = "raw_csv"
 dest_cleaned = "pre_cleaned"
 folders_all = [k for k in os.listdir(source) if "Comp" in k or "Filter" in k]
 
-company_folders = [k for k in folders_all if "Comp" in k]
+company_folders = ["Companies_en", "Companies_de"]
 nofilter_folders = [k for k in folders_all if "Filter" in k]
+
+# get all company folders without en/de
+subfolders = [k.split("_")[0] for k in  os.listdir(os.path.join(source, "Companies_en"))]
+
+
+
 
 #%% testing
 folder = nofilter_folders[0]
@@ -71,47 +90,142 @@ folder = company_folders[0]
 subfolder = subfolders[0]
 
 #%%
-for main_folder in folders_all:
-    if main_folder in company_folders:
-        subfolders = os.listdir(os.path.join(source,folder))
-        for subfolder in subfolders:
-            new_dest = os.path.join(dest,folder, subfolder)
+
+# list all subfolders
+
+for subfolder in subfolders:
+    print(f"Working on {subfolder}")
+    new_dest = os.path.join(dest,"Companies", subfolder)
+    new_dest_cleaned = os.path.join(dest_cleaned,"Companies", subfolder)
     
-            if not os.path.exists(new_dest):
-                os.mkdir(os.path.join(new_dest))
-            files = os.listdir(os.path.join(source,folder, subfolder))
-            for file in [k for k in files if ".json" in k]:
-                
-                tweets = []
-                path = os.path.join(source,folder,subfolder, file)
-                for line in open(path, 'r',encoding="utf8"):
-                    tweets.append(json.loads(line,parse_int=str))
-                df = pd.DataFrame(tweets)
-                
-                # fix place column
-                # split into two new lat/long columns
-                if ~sum(df["place"] == "") == len(df):
-                    
-                    coord = df["place"].apply(pd.Series)["coordinates"].swifter.progress_bar(False).apply(pd.Series)
-                    coord.rename(columns = {0:"lat", 1:"long"},
-                         inplace = True)
-                    df = pd.concat([df, coord], axis = 1)
+    # create folder in new destination if it does not already exist
+    if not os.path.exists(new_dest):
+        os.mkdir(os.path.join(new_dest))
+    if not os.path.exists(new_dest_cleaned):
+        os.mkdir(os.path.join(new_dest_cleaned))
+    
+    # now go into each company folder in the source an concat files from same day together
+    # for this need to check if files exist in both and control for it
+    files_de = os.listdir(os.path.join(source, "Companies_de", f"{subfolder}_de"))
+    files_en = os.listdir(os.path.join(source, f"Companies_en", f"{subfolder}_en"))
+    
+
+    # get the dates available in both datasets
+    dates_de = [re.search(r'\d{4}-\d{2}-\d{2}', file).group() for file in files_de]
+    dates_en = [re.search(r'\d{4}-\d{2}-\d{2}', file).group() for file in files_en]
+    dates_both = list(set(dates_de) & set(dates_en))
+    
+    # now for each date available in both got thru both folders and concat files, then clean and save them
+    for date in dates_both:
+        # go into englisch folder
+        tweets = []
+        for folder in company_folders:
+            file = f"{subfolder}_{date}_{folder.split('_')[1]}.json"
+            path = os.path.join(source,folder,f"{subfolder}_{folder.split('_')[1]}", file)
+            for line in open(path, 'r',encoding="utf8"):
+                tweets.append(json.loads(line,parse_int=str))
+        
+        # convert to df
+        df = pd.DataFrame(tweets)
+        
+        # clean df
+        df = df_cleaner(df)
+        
+        # save df
+        new_filename_csv = f"{subfolder}_{date}.csv"
+        
+        # save df
+        print("Saving data in both")
+        df.to_csv(os.path.join(new_dest ,new_filename_csv),
+                  index = False)
+        
+        
+        ###########
+        # now replace emojis and save in different destination
+        ###########
+        df["tweet"] = df["tweet"].swifter.progress_bar(False).apply(lambda tweet: demoji.replace_with_desc(tweet, 
+                                                                                           sep = " "))# replace _ from emojis with " "
+        df.tweet = df.tweet.str.replace("_", " ")
+        #save df
+        df.to_csv(os.path.join(new_dest_cleaned,new_filename_csv),
+                  index = False)
+    
+    
+    # now continue for dates not in both
+    dates_de_only = list(set(dates_de) - set(dates_en))
+    
+    
+    # only clean german files
+    for date in dates_de_only:
+        tweets = []
+        file = f"{subfolder}_{date}_de.json"
+        path = os.path.join(source,"Companies_de",f"{subfolder}_de", file)
+        for line in open(path, 'r',encoding="utf8"):
+            tweets.append(json.loads(line,parse_int=str))
             
-                else:
-                    df["lat"] = df["long"] = None
-                df.drop("place", axis = 1, inplace = True)
-                
-                
-                # only select needed columns
-                df = df[["id", "tweet", "created_at", "user_id", 
-                         "username", "hashtags", "lat", "long", 
-                         "language", "replies_count", "retweets_count", 
-                         "likes_count"]]
-                
-                
-                new_filename = f"{file.split('.')[0]}.csv"
-                df.to_csv(os.path.join(new_dest,new_filename))    
-                
+         # convert to df
+        df = pd.DataFrame(tweets)
+        
+        # clean df
+        df = df_cleaner(df)
+        
+        # save df
+        new_filename_csv = f"{subfolder}_{date}.csv"
+        
+        # save df
+        print("Saving german data")
+        df.to_csv(os.path.join(new_dest ,new_filename_csv),
+                  index = False)
+        
+        ###########
+        # now replace emojis and save in different destination
+        ###########
+        df["tweet"] = df["tweet"].swifter.progress_bar(False).apply(lambda tweet: demoji.replace_with_desc(tweet, 
+                                                                                           sep = " "))# replace _ from emojis with " "
+        df.tweet = df.tweet.str.replace("_", " ")
+        #save df
+        df.to_csv(os.path.join(new_dest_cleaned,new_filename_csv),
+                  index = False)
+    
+    # same for englisch only
+     # now continue for dates not in both
+    dates_en_only = list(set(dates_en) - set(dates_de))
+    
+    
+    # only clean german files
+    for date in dates_en_only:
+        tweets = []
+        file = f"{subfolder}_{date}_en.json"
+        path = os.path.join(source,"Companies_en",f"{subfolder}_en", file)
+        for line in open(path, 'r',encoding="utf8"):
+            tweets.append(json.loads(line,parse_int=str))
+            
+         # convert to df
+        df = pd.DataFrame(tweets)
+        
+        # clean df
+        df = df_cleaner(df)
+        
+        # save df
+        new_filename_csv = f"{subfolder}_{date}.csv"
+        
+        # save df
+        print("Saving english data")
+        df.to_csv(os.path.join(new_dest ,new_filename_csv),
+                  index = False)  
+    
+        ###########
+        # now replace emojis and save in different destination
+        ###########
+        df["tweet"] = df["tweet"].swifter.progress_bar(False).apply(lambda tweet: demoji.replace_with_desc(tweet, 
+                                                                                           sep = " "))# replace _ from emojis with " "
+        df.tweet = df.tweet.str.replace("_", " ")
+        #save df
+        df.to_csv(os.path.join(new_dest_cleaned,new_filename_csv),
+                  index = False)
+        
+    
+    
         
             
     
@@ -122,6 +236,8 @@ for main_folder in folders_all:
         
     
             
+
+
 #%%  process for all NoFilter folders
 # for nofilter folders concat all tweets from one day together
 
@@ -147,26 +263,7 @@ first_update = min(dates_list)
 
 date_list_needed = pd.date_range(start=first_update,end=last_update).strftime('%Y-%m-%d').to_list()
 
-#%% testing
-date = date_list_needed[7]
-folder = en_folders[5]
-subfolder = [k for k in en_folders if k != folder][0]
-
-
-
-#%% cleaning functions
-def remove_light(text):
-        reference_pattern = re.compile(r"\@\w+|\@\w+\'|\@\w+\’|<.*?>|https?://\S+|www\.\S+|\d+\.|\d+|\#|\&amp|RT")
-        return reference_pattern.sub(r'', text)
-
-print(remove_light('''Everybody in the UK is flipping out over a video of some 
-                   dude picking up a mace, and I cannot for the life of me 
-                   understand why.   Is it because you’re playing by 1st ed. 
-                   rules and he’s not a cleric? lukas@email.com @donaldtup'''))
-
-   
-
-#%%
+#%% process for nofilter folders
 # for each date go into all folders concat the dfs, clean them and store them inside new folder
 
 for lang in lang_folders:
@@ -178,11 +275,12 @@ for lang in lang_folders:
         
     # go thru all dates
     for date in date_list_needed:
+        
         # set up list
         tweets = []
         #go into each folder folders an concat tweets to df
         for folder in folders:
-            
+            print(f"Working in {date} {folder}")
             # create filename from fodler name together wit date
             filename = f"{folder}_{date}.json"
             # create path
@@ -196,7 +294,8 @@ for lang in lang_folders:
         df = pd.DataFrame(tweets)
         
         # clean dataframe
-        df = df_cleaner(df)
+        print("Cleaning df")
+        df = df_cleaner(df, lang_controller = True)
         
         
         
@@ -204,6 +303,7 @@ for lang in lang_folders:
         new_filename_csv = f"{lang}_NoFilter_{date}.csv"
         
         # save df
+        print("Saving data")
         df.to_csv(os.path.join(dest, f"{lang}_NoFilter",new_filename_csv),
                   index = False)
         
