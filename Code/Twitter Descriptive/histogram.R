ef0ef12e4e1b9ea1a77bcd90a51a668d1b93cce3
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-02-24 00:45:19 +0100

    Update histogram.R
    modified functions to work on entire dataset

--- Code/Twitter Descriptive/histogram.R
+++ Code/Twitter Descriptive/histogram.R
@@ -14,13 +14,13 @@ long_list <- c(0,81)
 folder <- folders[3]
 file <- files[1]
 retweets <- 0
-likes <- 5
-long <- 0
+likes <- 0
+longs <- 0
 
 # read data
 files <- list.files(source)
 
-
+df <- rbind(df, df2)
 
 df <- readr::read_csv(file.path(source,file),
                col_types = cols_only(doc_id = "c",text = "c",
@@ -28,33 +28,54 @@ df <- readr::read_csv(file.path(source,file),
                                      retweets_count = "i",
                                      likes_count = "i", tweet_length = "i")) 
 
+df$date <- as.Date(df$created_at)
 
 # for retweets hist
-df_bins <- df %>% filter(
-  likes_count >= likes,
-  retweets_count >= retweets,
-  #long_tweet == long
-  tweet_length >= long) %>%
+df_bins_rt <- df %>% filter(
+  likes_count >= likes &
+  retweets_count >= retweets &
+  tweet_length >= longs) %>%
   group_by(date, retweets_count) %>%
-  summarise(n = n())
+  summarise(n = n())  %>%
+  pivot_wider(names_from = retweets_count, values_from = n)
 
 # for likes histo
-df_bins <- df %>% filter(
-  likes_count >= likes,
-  retweets_count >= retweets,
+df_bins_likes <- df %>% filter(
+  likes_count >= likes &
+  retweets_count >= retweets &
   #long_tweet == long
-  tweet_length >= long) %>%
+  tweet_length >= longs) %>%
   group_by(date, likes_count) %>%
-  summarise(n = n())
+  summarise(n = n()) %>%
+  pivot_wider(names_from = likes_count, n)
 
 # for long histo
-df_bins <- df %>% filter(
-  likes_count >= likes,
-  retweets_count >= retweets,
+df_bins_long <- df %>% filter(
+  likes_count >= likes &
+  retweets_count >= retweets &
   #long_tweet == long
-  tweet_length >= long) %>%
+  tweet_length >= longs) %>%
   group_by(date, tweet_length) %>%
-  summarise(n = n())
+  summarise(n = n())  %>%
+  pivot_wider(tweet_length, n)
+
+
+# get data for time series of means and counts for retweets, likes, length_tweet and number of tweets
+df_sum_stats <- df %>% filter(
+  likes_count >= likes &
+  retweets_count >= retweets &
+  #long_tweet == long
+  tweet_length >= longs) %>%
+  group_by(date) %>%
+  summarise(mean_rt = mean(retweets_count),
+            mean_likes = mean(likes_count),
+            mean_length = mean(tweet_length),
+            count_tweetts = n())  %>%
+  mutate(retweets_count = retweets,
+         likes_count = likes,
+         tweet_length = longs)
+
+
 
 
 # plot
