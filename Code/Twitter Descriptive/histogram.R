f58d8e008192bad73cde7d588c60eb94308f01d6
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-02-24 17:48:37 +0100

    Update histogram.R
    modified process for entire df and checked how to plot with final data

--- Code/Twitter Descriptive/histogram.R
+++ Code/Twitter Descriptive/histogram.R
@@ -11,76 +11,124 @@ long_list <- c(0,81)
 
 
 #### for testing
-folder <- folders[3]
+folder <- folders[2]
+# read data
+files <- list.files(file.path("cleaned", folder))
+files <- files[1:10]
 file <- files[1]
 retweets <- 0
 likes <- 0
 longs <- 0
 
-# read data
-files <- list.files(source)
 
-df <- rbind(df, df)
 
-df <- readr::read_csv(file.path(source,file),
+df_all <- NULL
+for (file in files){
+df <- readr::read_csv(file.path("cleaned", folder ,file),
                col_types = cols_only(doc_id = "c",text = "c",
                                      created_at = "c",
                                      retweets_count = "i",
-                                     likes_count = "i", tweet_length = "i")) 
+                                     likes_count = "i", tweet_length = "i",
+                                     language = "c")) 
+if(is.null(df_all)){
+  df_all <- df
+} else {
+df_all <- rbind(df_all, df)
+}
+}
 
-df$date <- as.Date(df$created_at)
+
+
+df_all$date <- as.Date(df_all$created_at, "%Y-%m-%d")
+
+# histo
+df_all %>% filter(
+  likes_count >= likes &
+    retweets_count >= retweets &
+    tweet_length >= longs,
+  date >= "2018-11-30" & date <= "2019-12-07") %>%
+  ggplot() +
+  geom_histogram(aes(retweets_count))
+
+
 
 # for retweets hist
-df_bins_rt <- df %>% filter(
+df_bins_rt <- df_all %>% filter(
   likes_count >= likes &
   retweets_count >= retweets &
   tweet_length >= longs) %>%
-  group_by(date, retweets_count) %>%
+  group_by(date,language, retweets_count) %>%
   summarise(n = n())  %>%
-  pivot_wider(names_from = retweets_count, values_from = n)
+  pivot_wider(names_from = retweets_count, values_from = n) %>%
+  mutate(retweets_count = retweets,
+         likes_count = likes,
+         tweet_length = longs)%>%
+  ungroup()
 
 # for likes histo
-df_bins_likes <- df %>% filter(
+df_bins_likes <- df_all %>% filter(
   likes_count >= likes &
   retweets_count >= retweets &
   #long_tweet == long
   tweet_length >= longs) %>%
-  group_by(date, likes_count) %>%
+  group_by(date,language, likes_count) %>%
   summarise(n = n()) %>%
-  pivot_wider(names_from = likes_count, n)
+  pivot_wider(names_from = likes_count,values_from = n) %>%
+  mutate(retweets_count = retweets,
+         likes_count = likes,
+         tweet_length = longs)%>%
+  ungroup()
 
 # for long histo
-df_bins_long <- df %>% filter(
+df_bins_long <- df_all %>% filter(
   likes_count >= likes &
   retweets_count >= retweets &
   #long_tweet == long
   tweet_length >= longs) %>%
-  group_by(date, tweet_length) %>%
+  group_by(date,language, tweet_length) %>%
   summarise(n = n())  %>%
-  pivot_wider(tweet_length, n)
+  pivot_wider(names_from = tweet_length,values_from =  n) %>%
+  mutate(retweets_count = retweets,
+         likes_count = likes,
+         tweet_length = longs)%>%
+  ungroup()
 
 
 # get data for time series of means and counts for retweets, likes, length_tweet and number of tweets
-df_sum_stats <- df %>% filter(
+df_sum_stats <- df_all %>% filter(
   likes_count >= likes &
   retweets_count >= retweets &
   #long_tweet == long
   tweet_length >= longs) %>%
-  group_by(date) %>%
+  group_by(date,language) %>%
   summarise(mean_rt = mean(retweets_count),
             mean_likes = mean(likes_count),
             mean_length = mean(tweet_length),
             count_tweetts = n())  %>%
   mutate(retweets_count = retweets,
          likes_count = likes,
-         tweet_length = longs)
+         tweet_length = longs) %>%
+  ungroup()
 
 
 
 
-# plot
-df %>%
-ggplot(aes(Var1, Freq)) +
+# plot histo in app
+df_bins_long %>%
+  filter(date >= "2018-11-30" & date <= "2019-12-07" &
+         language == "de" &
+           likes_count >= likes &
+           retweets_count >= retweets &
+           #long_tweet == long
+           tweet_length >= longs) %>% 
+  select(-c(date, language, retweets_count, likes_count, tweet_length)) %>%
+
+
+
+  colSums(na.rm = T) %>% data.frame() %>% rownames_to_column() %>%
+  rename(bins = rowname, n = ".") %>%
+  mutate(bin = cut_interval(bins, n = 200))
+ggplot(aes(bins, n)) +
   geom_col()
 
 for (folder in folders){
