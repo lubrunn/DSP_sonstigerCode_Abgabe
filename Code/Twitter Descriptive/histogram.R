7496f675999d38f7eac50bd8ea1a0cc5f327da74
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-02-24 19:45:27 +0100

    Update histogram.R
    created functions for histograms and time series

--- Code/Twitter Descriptive/histogram.R
+++ Code/Twitter Descriptive/histogram.R
@@ -25,7 +25,7 @@ longs <- 0
 df_all <- NULL
 for (file in files){
 df <- readr::read_csv(file.path("cleaned", folder ,file),
-               col_types = cols_only(doc_id = "c",text = "c",
+               col_types = cols_only(
                                      created_at = "c",
                                      retweets_count = "i",
                                      likes_count = "i", tweet_length = "i",
@@ -38,98 +38,88 @@ df_all <- rbind(df_all, df)
 }
 
 
-
+# add date column
 df_all$date <- as.Date(df_all$created_at, "%Y-%m-%d")
 
-# histo
-df_all %>% filter(
-  likes_count >= likes &
-    retweets_count >= retweets &
-    tweet_length >= longs,
-  date >= "2018-11-30" & date <= "2019-12-07") %>%
-  ggplot() +
-  geom_histogram(aes(retweets_count))
 
-
-
+'
+ function that aggreagtes data on a daily basis based on several filter
+ this will be used to create histograms for retweets, likes and tweet length
+ this is done for preprocessing because live computation may take very long otherwise
+ depending on chosen filter, e.g. for filter that take out a lot of data (e.g. retweets > 200)
+ the live computation would be somewhat fast enough but for broad filter (retweets  >0)
+ computation would take up to 1 minute per histogram
+'
+hist_data_creator <- function(df, retweets_filter, likes_filter, length_filter, grouping_variable){
 # for retweets hist
-df_bins_rt <- df_all %>% filter(
-  likes_count >= likes &
-  retweets_count >= retweets &
-  tweet_length >= longs) %>%
-  group_by(date,language, retweets_count) %>%
+df <- df_all %>% filter(
+  # filter out accoring to loop
+  likes_count >= likes_filter &
+  retweets_count >= retweets_filter &
+  tweet_length >= length_filter) %>%
+  # count number of tweets per bin in likes, length, retweets
+  group_by(date,language, .data[[grouping_variable]]) %>%
   summarise(n = n())  %>%
-  pivot_wider(names_from = retweets_count, values_from = n) %>%
+  # spread dataframe
+  pivot_wider(names_from = .data[[grouping_variable]], values_from = n) %>%
+  # add values that were used to filter for later filtering
   mutate(retweets_count = retweets,
          likes_count = likes,
-         tweet_length = longs)%>%
+         tweet_length = longs,
+         # count number of tweets that are part of one aggregated day
+         tweet_number = sum(c_across(-c("retweets_count",
+                                        "likes_count", "tweet_length")), na.rm = T))%>%
   ungroup()
+return(df)
+}
 
-# for likes histo
-df_bins_likes <- df_all %>% filter(
-  likes_count >= likes &
-  retweets_count >= retweets &
-  #long_tweet == long
-  tweet_length >= longs) %>%
-  group_by(date,language, likes_count) %>%
-  summarise(n = n()) %>%
-  pivot_wider(names_from = likes_count,values_from = n) %>%
-  mutate(retweets_count = retweets,
-         likes_count = likes,
-         tweet_length = longs)%>%
-  ungroup()
+'
+function that aggregates by day and language and takes the means of likes,
+retweets and tweet_length --> will be used for plotting time series of
+the means according to several filters
+'
 
-# for long histo
-df_bins_long <- df_all %>% filter(
-  likes_count >= likes &
-  retweets_count >= retweets &
-  #long_tweet == long
-  tweet_length >= longs) %>%
-  group_by(date,language, tweet_length) %>%
-  summarise(n = n())  %>%
-  pivot_wider(names_from = tweet_length,values_from =  n) %>%
-  mutate(retweets_count = retweets,
-         likes_count = likes,
-         tweet_length = longs)%>%
-  ungroup()
+sum_stats_creator <- function(df_all, retweets_filter, likes_filter, length_filter){
+  df <- df_all %>% filter(
+    likes_count >= likes_filter &
+      retweets_count >= retweets_filter &
+      #long_tweet == long
+      tweet_length >= length_filter) %>%
+    group_by(date,language) %>%
+    summarise(mean_rt = mean(retweets_count),
+              mean_likes = mean(likes_count),
+              mean_length = mean(tweet_length),
+              count_tweeets = n())  %>%
+    mutate(retweets_count = retweets,
+           likes_count = likes,
+           tweet_length = longs) %>%
+    ungroup()
+  return(df)
+  
+}
 
+df_bins_rt <- hist_data_creator(df_all,retweets_filter =  retweets,
+                                likes_filter = likes,
+                                length_filter = longs,
+                                grouping_variable = "retweets_count")
 
-# get data for time series of means and counts for retweets, likes, length_tweet and number of tweets
-df_sum_stats <- df_all %>% filter(
-  likes_count >= likes &
-  retweets_count >= retweets &
-  #long_tweet == long
-  tweet_length >= longs) %>%
-  group_by(date,language) %>%
-  summarise(mean_rt = mean(retweets_count),
-            mean_likes = mean(likes_count),
-            mean_length = mean(tweet_length),
-            count_tweetts = n())  %>%
-  mutate(retweets_count = retweets,
-         likes_count = likes,
-         tweet_length = longs) %>%
-  ungroup()
+df_bins_likes <- hist_data_creator(df_all, retweets, likes, longs, "likes_count")
+
+df_bins_long <- hist_data_creator(df_all, retweets, likes, longs, "tweet_length")
 
 
 
 
-# plot histo in app
-df_bins_long %>%
-  filter(date >= "2018-11-30" & date <= "2019-12-07" &
-         language == "de" &
-           likes_count >= likes &
-           retweets_count >= retweets &
-           #long_tweet == long
-           tweet_length >= longs) %>% 
-  select(-c(date, language, retweets_count, likes_count, tweet_length)) %>%
+df_sum_stats_n <- sum_stats_creator(df_all,retweets, likes, longs)
 
 
 
-  colSums(na.rm = T) %>% data.frame() %>% rownames_to_column() %>%
-  rename(bins = rowname, n = ".") %>%
-  mutate(bin = cut_interval(bins, n = 200))
-ggplot(aes(bins, n)) +
-  geom_col()
+
+
+
+
+
+
 
 for (folder in folders){
   if (grepl("Companies", folder)) {
