01be4218af1d5b14d5eff657e0e9c293c5b6b379
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-02-25 01:08:12 +0100

    Update append.R
    added loops to thru all filter

--- Code/Twitter Descriptive/append.R
+++ Code/Twitter Descriptive/append.R
@@ -1,6 +1,7 @@
 
 '
-Here we write a function that goes through a main model and appends for each subfolder the files fo each day into on big csv
+Here we write a function that goes through a main model and appends for each 
+subfolder the files fo each day into on big csv
 this csv will be used for sql, term_freq calc, histogram calc and maybe more
 '
 
@@ -78,7 +79,16 @@ appender <- function(files, source, dest, folder, companies = F){
     
 }
 
-company_appender <- function(source_main, company_folders){
+
+
+'
+this function appends all files that have alreday been appended on a 
+company level, goal is to have one df for all company tweets
+
+'
+
+
+company_appender <- function(source_main, dest, folder, company_folder){
   df_all <- NULL
   for (company_folder in company_folders){
     # read entire df for company
@@ -97,43 +107,71 @@ company_appender <- function(source_main, company_folders){
     } else {
       df_all <- rbind(df_all, df)
     }
+    
+    ### save df
+    file_path <- file.path(dest,  glue("{folder}_all.csv"))
+    write_csv(df_all, file_path)
   }
 }
   
   
 # this function goes thru all folders and calls the appender
 append_all <- function(source_main){
+  
+  
+  # list all folders in source main
   folders <- list.files(source_main)
+  
+  # go into each folder
   for (folder in folders){
+    
+    # if its the company folder
     if (grepl("Companies", folder)) {
+      
+      # path to company main folder
       source_all_comp <- file.path(source_main, folder)
+      # list all company folder
       company_folders <- list.files(source_all_comp)
       
+      # for each company folder go in 
       for (company_folder in company_folders){
-        source <- file.path(source_main, folder, company_folder)
-        dest <- file.path(source_main, folder, company_folder)
         
-        # list all files in folder
+        # path to individual company folder
+        source <- file.path(source_main, folder, company_folder)
+        # same as source just created for clarity (only string no long comp time)
+        dest <- file.path(source_main, "appended/Companies")
+        
+        # list all files in indivdual company folder
         files <- list.files(source)
+        
+        # append each file togehter and save the appended file in the destination
         appender(files, source, dest, folder = company_folder, companies = T)
       }
       
-      # then go into each company folder, take the appended df containing all days and append all company df to one big 
-      # company df containing all days for all companies
+      # then take each appended company df containing all 
+      # days and append all company df to one big company df containing all 
+      # days for all companies
       for (company_folder in company_folders){
-        
+        dest <- file.path(source_main, "appended/Companies")
+        company_appender(source_main = source_main, dest = dest,
+                         folder = "Companies", company_folder = company_folder)
       }
       
       
       
-      
+      # for all no filter folders go on
     } else if (grepl("NoFilter", folder)) {
-      source <- file.path(source_main, folder)
-      dest <- file.path(source_main, folder)
       
+      # path to en_nofilter/de_nofilter
+      source <- file.path(source_main, folder)
+      # destination
+      dest <- file.path(source_main, "appended")
+      
+      # list all files in the source
       files <- list.files(source)
       
-      # call function for each nofilter folder
+      # call function for each nofilter folder, append every single file and
+      # save results in dest
       appender(files, source, dest, folder, companies = F)
     }
   }
