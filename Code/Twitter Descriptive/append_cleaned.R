b75a851085b9990c6118877b03d3a5597a654538
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-02-27 21:05:15 +0100

    Update append_cleaned.R

--- Code/Twitter Descriptive/append_cleaned.R
+++ Code/Twitter Descriptive/append_cleaned.R
@@ -2,21 +2,3 @@ soure("append.R")
 
 
 
-# which folders should be appended
-source_main <- "cleaned"
-
-# list all folders in source main
-folders <- list.files(source_main)
-
-folders <- folders[5]
-
-# start function
-append_all(source_main, folders)
-
-
-
-dest <- "appended"
-lang = "En"
-
-
-final_appender(source_main,dest = "appended", "En")
