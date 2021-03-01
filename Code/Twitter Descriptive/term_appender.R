921ceafca64deb82a38d90d9df879b383dd8b639
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-03-02 00:00:53 +0100

    Create term_appender.R
    appends all 40k individual term frequency files, only for nofilter so far

---
+++ Code/Twitter Descriptive/term_appender.R
@@ -0,0 +1,71 @@
+
+library(vroom)
+library(glue)
+
+
+
+
+
+
+
+################################################################################
+### this is simply for easier switching between vpcl and local
+vpc = FALSE
+
+# read in data
+if (vpc == T) {
+  setwd("/home/lukasbrunner/share/onedrive_new/Data/Twitter")
+} else {
+  setwd("C:/Users/lukas/OneDrive - UT Cloud/Data/Twitter")
+}
+
+
+
+source_main <- "term_freq"
+
+folders_main <- list.files(source_main)[!grepl("appended", list.files(source_main))]
+
+
+likes_list <- c(0, 10, 50, 100, 200)
+retweets_list <- c(0, 10, 50, 100, 200)
+long_list <- c(0,81)
+
+#### process for nofilter folders
+for (folder in folders_main){
+  folders_sub <- list.files(file.path(source_main, folder))
+  
+  for (subfolder in folders_sub[!grepl("appended", folders_sub)])
+    
+    # list all files
+    files <- list.files(file.path(source_main, folder, subfolder))
+  
+  for (retweets_filter in retweets_list){
+    for(likes_filter in likes_list){
+      for(length_filter in long_list){
+        # check which name to five file
+        if (length_filter == 81){
+          long_name <- "long_only"
+        } else{
+          long_name <- "all"
+        }
+        
+        add_on <- glue("rt_{retweets_filter}_li_{likes_filter}_lo_{long_name}")
+        # check all files that have this addon
+        selected_files <-  files[grepl(add_on, files)] 
+        
+        # read all files and append
+        df <- vroom:vroom(file.path(source_main, folder, subfolder, selected_files))
+        
+        new_filename <- glue("{subfolder}_{folder}_{add_on}.csv")
+        dest <- file.path(source_main,folder, glue("{subfolder}_appended", ))
+        
+        vroom_write(df, dest, delim =",")
+        
+        
+      }
+    }
+  }
+}
+
+
+
