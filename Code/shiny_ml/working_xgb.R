60dd72e45c3cc685f393a70d8dc9595b2dc0865c
Simon Hassler <simon.hassler@student.uni-tuebingen.de>
2021-03-03 23:44:05 +0100

    Update working_xgb.R

--- Code/shiny_ml/working_xgb.R
+++ Code/shiny_ml/working_xgb.R
@@ -8,10 +8,12 @@ library(forecast)
 library(caret)
 library(dygraphs)
 #notes:
-#scaling
-
+#scaling, hypeparameter tuning
+#preprocessing in shiny -> find good amount of ARs and MAs
+# corona dummy
 #########################################
 #shiny structure
+#ggAcf, ggPacf
 #dynamic variables
   #split train test (in percent):
 split <- 0.7
@@ -50,6 +52,13 @@ split_data <- function(sample,split){
 
     return(out)
 }
+
+#do it after loading covariates and stocks
+preprocess.df.train = preProcess(df.train, method=c("scale","center","BoxCox"))
+df.train = predict(preprocess.df.train, newdata = df.train)
+df.test = predict(preprocess.df.train,newdata = df.test)
+
+#https://stackoverflow.com/questions/57924064/r-descale-data-back-to-their-original-values
 
 #####do it for df.train
 #create AR features
@@ -144,6 +153,7 @@ pred <- xgboost::xgb.DMatrix(data = as.matrix(df.test[,-c(1)]))
 test_pred <- predict(model,pred)
 
 # 1. Plot
+#https://towardsdatascience.com/how-to-create-better-interactive-forecast-plots-using-r-and-dygraph-29bdd7146066
 test_pred <- test_pred %>%
   zoo(seq(from = as.Date(min(date.test)), to = as.Date(max(date.test)), by = "day"))
 ts <- sample %>% pull(Close.) %>%
@@ -171,15 +181,18 @@ ts_train <- y.train %>%
 c <- {cbind(fitted=ts_train_fit, actual_train=ts_train,actual_test=ts_test,
             predicted=test_pred)} %>% dygraph()
 
-# RMSE , MAPE
+# RMSE , MAPE #white noise test
 
 rmse <-  sqrt(mean((test_pred - df.test$Close.)^2))
 
 mape <- mean(abs((df.test$Close.- test_pred)/df.test$Close.) * 100)
 
+# check mean 0 and correlation
+residuals = as.numeric(ts_train) - as.numeric(ts_train_fit)
 
+Box.test(residuals, lag=10, fitdf=0)
 
-
+ggAcf(residuals) + ggtitle("ACF of residuals")
 
 
 #forecast for future
@@ -216,7 +229,7 @@ data_forecast <- function(sample,df.train,length_forecast){
 pred_forecast <- xgboost::xgb.DMatrix(data = as.matrix(pred_forecast))
 forecast_ts <- predict(model,pred_forecast)
 
-# 4. Plot  complete TS and forecast
+# 4. Plot  forecast
 
 
 forecast_ts <- forecast_ts %>%
@@ -224,27 +237,163 @@ forecast_ts <- forecast_ts %>%
 
 upper <- forecast_ts + 1.96*sqrt(sigma_res)
 lower <- forecast_ts - 1.96*sqrt(sigma_res)
-residuals = as.numeric(ts_train) - as.numeric(ts_train_fit)
 sigma_res <- sd(residuals)
 
-forecast_ts <- forecast_ts %>%
-  zoo(seq(from = as.Date(max(date.test))+1, to = as.Date(max(date.test))+length_forecast, by = "day"))
-
-{cbind(actuals=ts, forecast_mean=xgb_forecast)} %>% dygraph()
-
-{cbind(actuals=ts, forecast_mean=xgb_forecast,
-         lower_95=lower, upper_95=upper)} %>%
-  dygraph()
+# full_ts <- sample %>% dplyr::select(date,Close.) %>%
+#   zoo(seq(from = as.Date(min(date.train)), to = as.Date(max(date.test)), by = "day"))
 
 
-{cbind(actuals=ts, forecast_mean=xgb_forecast,
+{cbind(forecast_mean=forecast_ts,
        lower_95=lower, upper_95=upper)} %>%
   dygraph() %>%
-  dySeries("Close.", color = "black") %>%
   dySeries(c("lower_95", "forecast_mean", "upper_95"),
            label = "95%", color = "blue")
 
 
+
+
+
+
+###############Hyperparameter tuning
+library(rsample)
+library(recipes)
+library(parsnip)
+library(tune)
+library(dials)
+library(workflows)
+library(yardstick)
+library(janitor)
+sample_t <- sample %>% select(-date)
+
+ames_split <- rsample::initial_split(
+  sample_t,
+  prop = 0.7,
+  strata = Close.
+)
+
+preprocessing_recipe <-
+  recipes::recipe(Close. ~ ., data = training(ames_split)) %>% prep()
+
+ames_cv_folds <-
+  recipes::bake(
+    preprocessing_recipe,
+    new_data = training(ames_split)
+  ) %>%
+  rsample::vfold_cv(v = 2)
+
+xgboost_model <-
+  parsnip::boost_tree(
+    mode = "regression",
+    trees = 100,
+    min_n = tune(),
+    tree_depth = tune(),
+    learn_rate = tune(),
+    loss_reduction = tune()
+  ) %>%
+  set_engine("xgboost", objective = "reg:squarederror")
+
+xgboost_params <-
+  dials::parameters(
+    min_n(),
+    tree_depth(),
+    learn_rate(),
+    loss_reduction()
+  )
+
+xgboost_grid <-
+  dials::grid_max_entropy(
+    xgboost_params,
+    size = 30
+  )
+
+
+xgboost_wf <-
+  workflows::workflow() %>%
+  add_model(xgboost_model) %>%
+  add_formula(Close. ~ .)
+
+xgboost_tuned <- tune::tune_grid(
+  object = xgboost_wf,
+  resamples = ames_cv_folds,
+  grid = xgboost_grid,
+  metrics = yardstick::metric_set(yardstick::rmse, yardstick::mae),
+  control = tune::control_grid(verbose = TRUE)
+)
+
+xgboost_tuned %>%
+  tune::show_best(metric = "rmse") %>%
+  knitr::kable()
+
+xgboost_best_params <- xgboost_tuned %>%
+  tune::select_best("rmse")
+knitr::kable(xgboost_best_params)
+
+xgboost_model_final <- xgboost_model %>%
+  finalize_model(xgboost_best_params)
+
+
+
+test_processed  <- bake(preprocessing_recipe, new_data = testing(ames_split))
+train_processed <- bake(preprocessing_recipe,  new_data = training(ames_split))
+
+test_prediction <- xgboost_model_final %>%
+  # fit the model on all the training data
+  fit(
+    formula = Close. ~ .,
+    data    = train_processed
+  ) %>%
+  # use the training model fit to predict the test data
+  predict(new_data = test_processed) %>%
+  bind_cols(testing(ames_split))
+
+# measure the accuracy of our model using `yardstick`
+
+test_prediction$Close. <- as.numeric(test_prediction$Close.)
+
+xgboost_score <-
+  test_prediction %>%
+  yardstick::metrics(Close., .pred) %>%
+  mutate(.estimate = format(round(.estimate, 2), big.mark = ","))
+
+knitr::kable(xgboost_score)
+
+house_prediction_residual <- test_prediction %>%
+  arrange(.pred) %>%
+  mutate(residual_pct = (Close. - .pred)) %>%
+  select(.pred, residual_pct)
+
+ggplot(house_prediction_residual, aes(x = .pred, y = residual_pct)) +
+  geom_point() +
+  xlab("Predicted Sale Price") +
+  ylab("Residual")
+
+
+
+Box.test(house_prediction_residual$residual_pct, lag=10, fitdf=0)
+
+ggAcf(house_prediction_residual) + ggtitle("ACF of residuals")
+
+
+
+a <- xgboost_model_final %>%
+  # fit the model on all the training data
+  fit(
+    formula = Close. ~ .,
+    data    = train_processed
+  ) %>%
+  # use the training model fit to predict the test data
+  predict(new_data = pred_forecast)
+
+
+names(pred_forecast)
+
+pred_forecast$Close..2 <- NULL
+
+pred_forecast$Close..1 <- NULL
+
+pred_forecast$date.1 <- NULL
+
+pred_forecast$MA <- NULL
 
 
 
